2bd3b265346cec818a525ecc82e6c6beee20a994
lucaskojo02 <lucaskojo02@gmail.com>
2025-06-19 17:42:52 +0300

    Added CSS to the elements

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -6,11 +6,10 @@ function ToDoList(){
     const [newTask,setNewTask] = useState("")
 
     const handleInputChange = (event)=>{
-
+        setNewTask(event.target.value)
     }
 
     const addTask = ()=>{
-
 
     }
 
@@ -26,7 +25,42 @@ function ToDoList(){
 
     }
 
-    return(<></>)
+    return(
+        <div className="to-do-list">
+            <h1>To-Do-List</h1>
+
+            <div>
+                <input type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange} />
+
+                <button className="add-button" onClick={addTask}>
+                    Add
+                </button>
+            </div>
+
+            <ol>
+                {tasks.map((task,index)=>{
+                    <li key={index}>
+                        <span className="text">{task}</span>
+                        <button className="delete-button" 
+                        onClick={()=>deleteTask(index)}>
+                            Delete
+                        </button>
+
+                        <button className="move-button" 
+                        onClick={()=>moveTaskUp(index)}>
+                            ☝
+                        </button>
+
+                        <button className="move-button" 
+                        onClick={()=>moveTaskDown(index)}>
+                            👇
+                        </button>
+                    </li>
+                })}
+            </ol>
+
+        </div>
+    )
 
 }
 
