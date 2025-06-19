a76ec51dc73b22b4cf0f8081f16879eadf44d10d
lucaskojo02 <lucaskojo02@gmail.com>
2025-06-19 16:54:55 +0300

    Declared the usefull functions

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,6 +2,32 @@ import React, {useState} from "react"
 
 function ToDoList(){
 
+    const [tasks, setTasks] = useState([]);
+    const [newTask,setNewTask] = useState("")
+
+    const handleInputChange = (event)=>{
+
+    }
+
+    const addTask = ()=>{
+
+
+    }
+
+    const deleteTask = (index)=>{
+
+    }
+
+    const moveTaskUp = (index) =>{
+
+    }
+
+    const moveTaskDown = (index) => {
+
+    }
+
+    return(<></>)
+
 }
 
 export default ToDoList;
